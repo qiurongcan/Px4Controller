051d504fa0841d1593ffa51c8145b1031b6645a8
qiurongcan <105793485+qiurongcan@users.noreply.github.com>
2026-03-19 11:47:47 +0800

    Refactor PX4CtrlParam.h and remove unused code

--- px4ctrl/src/PX4CtrlParam.h
+++ px4ctrl/src/PX4CtrlParam.h
@@ -1,98 +1,202 @@
-#ifndef __PX4CTRLPARAM_H
-#define __PX4CTRLPARAM_H
+// #ifndef __PX4CTRLFSM_H
+// #define __PX4CTRLFSM_H
+
+// #include <ros/ros.h>
+// #include <ros/assert.h>
+
+// #include <geometry_msgs/PoseStamped.h>
+// #include <nav_msgs/Odometry.h>
+// #include <mavros_msgs/SetMode.h>
+// #include <mavros_msgs/CommandLong.h>
+// #include <mavros_msgs/CommandBool.h>
+
+// #include "input.h"
+// // #include "ThrustCurve.h"
+// #include "controller.h"
+
+// struct AutoTakeoffLand_t
+// {
+// 	bool landed{true};
+// 	ros::Time toggle_takeoff_land_time;
+// 	std::pair<bool, ros::Time> delay_trigger{std::pair<bool, ros::Time>(false, ros::Time(0))};
+// 	Eigen::Vector4d start_pose;
+	
+// 	static constexpr double MOTORS_SPEEDUP_TIME = 3.0; // motors idle running for 3 seconds before takeoff
+// 	static constexpr double DELAY_TRIGGER_TIME = 2.0;  // Time to be delayed when reach at target height
+// };
+
+// class PX4CtrlFSM
+// {
+// public:
+// 	Parameter_t &param;
+
+// 	RC_Data_t rc_data;
+// 	State_Data_t state_data;
+// 	ExtendedState_Data_t extended_state_data;
+// 	Odom_Data_t odom_data;
+// 	Imu_Data_t imu_data;
+// 	Command_Data_t cmd_data;
+// 	Battery_Data_t bat_data;
+// 	Takeoff_Land_Data_t takeoff_land_data;
+
+// 	LinearControl &controller;
+
+// 	ros::Publisher traj_start_trigger_pub;
+// 	ros::Publisher ctrl_FCU_pub;
+// 	ros::Publisher debug_pub; //debug
+// 	ros::ServiceClient set_FCU_mode_srv;
+// 	ros::ServiceClient arming_client_srv;
+// 	ros::ServiceClient reboot_FCU_srv;
+
+// 	quadrotor_msgs::Px4ctrlDebug debug_msg; //debug
+
+// 	Eigen::Vector4d hover_pose;
+// 	ros::Time last_set_hover_pose_time;
+
+// 	enum State_t
+// 	{
+
+// 		MANUAL_CTRL = 1, // px4ctrl is deactived. FCU is controled by the remote controller only
+// 		AUTO_HOVER, // px4ctrl is actived, it will keep the drone hover from odom measurments while waiting for commands from PositionCommand topic.
+// 		CMD_CTRL,	// px4ctrl is actived, and controling the drone.
+// 		AUTO_TAKEOFF,
+// 		AUTO_LAND
+// 	};
+
+// 	PX4CtrlFSM(Parameter_t &, LinearControl &);
+// 	void process();
+// 	bool rc_is_received(const ros::Time &now_time);
+// 	bool cmd_is_received(const ros::Time &now_time);
+// 	bool odom_is_received(const ros::Time &now_time);
+// 	bool imu_is_received(const ros::Time &now_time);
+// 	bool bat_is_received(const ros::Time &now_time);
+// 	bool recv_new_odom();
+// 	State_t get_state() { return state; }
+// 	bool get_landed() { return takeoff_land.landed; }
+
+// private:
+// 	State_t state; // Should only be changed in PX4CtrlFSM::process() function!
+// 	AutoTakeoffLand_t takeoff_land;
+
+// 	// ---- control related ----
+// 	Desired_State_t get_hover_des();
+// 	Desired_State_t get_cmd_des();
+
+// 	// ---- auto takeoff/land ----
+// 	void motors_idling(const Imu_Data_t &imu, Controller_Output_t &u);
+// 	void land_detector(const State_t state, const Desired_State_t &des, const Odom_Data_t &odom); // Detect landing 
+// 	void set_start_pose_for_takeoff_land(const Odom_Data_t &odom);
+// 	Desired_State_t get_rotor_speed_up_des(const ros::Time now);
+// 	Desired_State_t get_takeoff_land_des(const double speed);
+
+// 	// ---- tools ----
+// 	void set_hov_with_odom();
+// 	void set_hov_with_rc();
+
+// 	bool toggle_offboard_mode(bool on_off); // It will only try to toggle once, so not blocked.
+// 	bool toggle_arm_disarm(bool arm); // It will only try to toggle once, so not blocked.
+// 	void reboot_FCU();
+
+// 	void publish_bodyrate_ctrl(const Controller_Output_t &u, const ros::Time &stamp);
+// 	void publish_attitude_ctrl(const Controller_Output_t &u, const ros::Time &stamp);
+// 	void publish_trigger(const nav_msgs::Odometry &odom_msg);
+// };
+
+// #endif
+
+
+
+
+#ifndef __PX4CTRLFSM_H
+#define __PX4CTRLFSM_H
 
 #include <ros/ros.h>
+#include <ros/assert.h>
 
-class Parameter_t
+#include <geometry_msgs/PoseStamped.h>
+#include <nav_msgs/Odometry.h>
+#include <mavros_msgs/SetMode.h>
+#include <mavros_msgs/CommandLong.h>
+#include <mavros_msgs/CommandBool.h>
+
+#include "input.h"
+#include "controller.h"
+
+struct AutoTakeoffLand_t
+{
+    bool landed{true};
+    ros::Time toggle_takeoff_land_time;
+    Eigen::Vector4d start_pose;
+};
+
+class PX4CtrlFSM
 {
 public:
-	struct Gain
-	{
-		double Kp0, Kp1, Kp2;
-		double Kv0, Kv1, Kv2;
-		double Kvi0, Kvi1, Kvi2;
-		double Kvd0, Kvd1, Kvd2;
-		double KAngR, KAngP, KAngY;
-	};
+    Parameter_t &param;
 
-	struct RotorDrag
-	{
-		double x, y, z;
-		double k_thrust_horz;
-	};
+    RC_Data_t rc_data;
+    State_Data_t state_data;
+    ExtendedState_Data_t extended_state_data;
+    Odom_Data_t odom_data;
+    Imu_Data_t imu_data;
+    Command_Data_t cmd_data;
+    Battery_Data_t bat_data;
+    Takeoff_Land_Data_t takeoff_land_data;
 
-	struct MsgTimeout
-	{
-		double odom;
-		double rc;
-		double cmd;
-		double imu;
-		double bat;
-	};
+    LinearControl &controller;
 
-	struct ThrustMapping
-	{
-		bool print_val;
-		double K1;
-		double K2;
-		double K3;
-		bool accurate_thrust_model;
-		double hover_percentage;
-	};
+    ros::Publisher traj_start_trigger_pub;
+    ros::Publisher ctrl_FCU_pub;
+    ros::Publisher debug_pub; 
+    ros::ServiceClient set_FCU_mode_srv;
+    ros::ServiceClient arming_client_srv;
+    ros::ServiceClient reboot_FCU_srv;
 
-	struct RCReverse
-	{
-		bool roll;
-		bool pitch;
-		bool yaw;
-		bool throttle;
-	};
+    quadrotor_msgs::Px4ctrlDebug debug_msg; 
 
-	struct AutoTakeoffLand
-	{
-		bool enable;
-		bool enable_auto_arm;
-		bool no_RC;
-		double height;
-		double speed;
-	};
+    // 用于悬停的目标位置
+    Eigen::Vector4d hover_pose;
+    ros::Time last_set_hover_pose_time;
+    
+    // 状态标记
+    bool takeoff_done{false}; // 标记是否完成起飞
 
-	Gain gain;
-	RotorDrag rt_drag;
-	MsgTimeout msg_timeout;
-	RCReverse rc_reverse;
-	ThrustMapping thr_map;
-	AutoTakeoffLand takeoff_land;
+    // --- 仿照 se3Ctrl 的状态枚举 ---
+    enum State_t
+    {
+        MANUAL_CTRL,   // 对应 WAITING_FOR_OFFBOARD (等待切入OFFBOARD)
+        AUTO_RUNNING,  // 对应 MISSION_EXECUTION (任务执行：起飞、悬停、轨迹)
+        AUTO_LAND      // 对应 LANDING (可选)
+    };
 
-	int pose_solver;
-	double mass;
-	double gra;
-	double max_angle;
-	double ctrl_freq_max;
-	double max_manual_vel;
-	double low_voltage;
-
-	bool use_bodyrate_ctrl;
-	// bool print_dbg;
-
-	Parameter_t();
-	void config_from_ros_handle(const ros::NodeHandle &nh);
-	void config_full_thrust(double hov);
+    PX4CtrlFSM(Parameter_t &, LinearControl &);
+    void process();
+    
+    // 辅助函数
+    bool rc_is_received(const ros::Time &now_time);
+    bool cmd_is_received(const ros::Time &now_time);
+    bool odom_is_received(const ros::Time &now_time);
+    bool imu_is_received(const ros::Time &now_time);
+    bool bat_is_received(const ros::Time &now_time);
+    bool recv_new_odom();
+    State_t get_state() { return state; }
 
 private:
-	template <typename TName, typename TVal>
-	void read_essential_param(const ros::NodeHandle &nh, const TName &name, TVal &val)
-	{
-		if (nh.getParam(name, val))
-		{
-			// pass
-		}
-		else
-		{
-			ROS_ERROR_STREAM("Read param: " << name << " failed.");
-			ROS_BREAK();
-		}
-	};
+    State_t state; 
+    AutoTakeoffLand_t takeoff_land;
+
+    Desired_State_t get_hover_des();
+    Desired_State_t get_cmd_des();
+
+    void set_hov_with_odom();
+    
+    bool toggle_offboard_mode(bool on_off); 
+    bool toggle_arm_disarm(bool arm); 
+    void reboot_FCU();
+    
+    void publish_bodyrate_ctrl(const Controller_Output_t &u, const ros::Time &stamp);
+    void publish_attitude_ctrl(const Controller_Output_t &u, const ros::Time &stamp);
+    void publish_trigger(const nav_msgs::Odometry &odom_msg);
 };
 
 #endif
